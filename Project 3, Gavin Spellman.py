8202530149c86ff7b2ba401a8b02968ba9a33b5f
g1spellman <g1spellman@student.bridgew.edu>
2025-10-01 08:11:02 -0400

    draft

---
+++ Project 3, Gavin Spellman.py
@@ -0,0 +1,22 @@
+import dearpygui.dearpygui as drawing
+import comp151Colors
+
+drawing.create_context()
+drawing.create_viewport(title='Face', width=900, height=900)
+with drawing.window(label='Face', width=900, height=900):
+    with drawing.drawlist(width=900, height=900):
+        drawing.draw_circle((450,450), 300,
+                         color=comp151Colors.RED, fill=comp151Colors.ORANGE)
+        drawing.draw_triangle((305,300), (395,410), (210,410),
+                              color=comp151Colors.BLACK, fill=comp151Colors.GREEN)
+        drawing.draw_triangle((650, 300),(740,410), (555,410),
+                              color=comp151Colors.BLACK, fill=comp151Colors.GREEN)
+        drawing.draw_rectangle((400,430), (500,460),
+                               color=comp151Colors.BLACK, fill=comp151Colors.GREEN)
+        drawing.draw_ellipse((300,550),(600,650),
+                             color=comp151Colors.RED, fill=comp151Colors.GREEN)
+
+drawing.setup_dearpygui()
+drawing.show_viewport()
+drawing.start_dearpygui()
+drawing.destroy_context()
