d0f56a86dc0d5b87f15fe82f052b78ebdb339179
g1spellman <g1spellman@student.bridgew.edu>
2025-10-01 15:08:57 -0400

    draft

--- Project 3, Gavin Spellman.py
+++ Project 3, Gavin Spellman.py
@@ -5,16 +5,36 @@ drawing.create_context()
 drawing.create_viewport(title='My Drawing', width=900, height=900)
 with drawing.window(label='My Drawing', width=900, height=900):
     with drawing.drawlist(width=900, height=900):
+        #Ears
         drawing.draw_triangle((300,0), (400,150), (210,150),
                               color=comp151Colors.BLACK, fill=comp151Colors.PINK)
         drawing.draw_triangle((600, 0),(700,150), (500,150),
                               color=comp151Colors.BLACK, fill=comp151Colors.PINK)
+        #Head
         drawing.draw_circle((450, 200), 100,
                             color=comp151Colors.BLACK, fill=comp151Colors.PINK)
+        #Nose
         drawing.draw_rectangle((400, 230), (500, 200),
                                color=comp151Colors.BLACK, fill=comp151Colors.BLACK)
-        drawing.draw_line((300,200), (300,500), color=comp151Colors.BLACK,
-                  thickness=2)
+        #Mouth
+        drawing.draw_line((400,250), (500,250), color=comp151Colors.BLACK,
+                  thickness=3)
+        #Eyes
+        drawing.draw_circle((400, 175), 25, color=comp151Colors.RED,
+                            fill=comp151Colors.BLACK)
+        drawing.draw_circle((500, 175), 25, color=comp151Colors.RED,
+                            fill=comp151Colors.BLACK)
+        drawing.draw_circle((400, 175), 15, color=comp151Colors.WHITE,
+                            fill=comp151Colors.WHITE)
+        drawing.draw_circle((500, 175), 15, color=comp151Colors.WHITE,
+                            fill=comp151Colors.WHITE)
+        #Feet
+        drawing.draw_ellipse((500, 500), (750, 600),color=comp151Colors.BLACK,
+                            fill = comp151Colors.PINK)
+        drawing.draw_ellipse((100, 500), (350, 600), color=comp151Colors.BLACK,
+                             fill=comp151Colors.PINK)
+        #Body
+
 
 drawing.setup_dearpygui()
 drawing.show_viewport()
