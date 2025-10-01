d8abcfb5d2175b431df72105d8eb6e1ac1b4623d
g1spellman <g1spellman@student.bridgew.edu>
2025-10-01 13:15:55 -0400

    draft

--- Project 3, Gavin Spellman.py
+++ Project 3, Gavin Spellman.py
@@ -2,19 +2,19 @@ import dearpygui.dearpygui as drawing
 import comp151Colors
 #lets draw a rabbit
 drawing.create_context()
-drawing.create_viewport(title='Face', width=900, height=900)
-with drawing.window(label='Face', width=900, height=900):
+drawing.create_viewport(title='My Drawing', width=900, height=900)
+with drawing.window(label='My Drawing', width=900, height=900):
     with drawing.drawlist(width=900, height=900):
-        drawing.draw_circle((450,450), 300,
-                         color=comp151Colors.RED, fill=comp151Colors.ORANGE)
-        drawing.draw_triangle((305,300), (395,410), (210,410),
-                              color=comp151Colors.BLACK, fill=comp151Colors.GREEN)
-        drawing.draw_triangle((650, 300),(740,410), (555,410),
-                              color=comp151Colors.BLACK, fill=comp151Colors.GREEN)
-        drawing.draw_rectangle((400,430), (500,460),
-                               color=comp151Colors.BLACK, fill=comp151Colors.GREEN)
-        drawing.draw_ellipse((300,550),(600,650),
-                             color=comp151Colors.RED, fill=comp151Colors.GREEN)
+        drawing.draw_triangle((300,0), (400,150), (210,150),
+                              color=comp151Colors.BLACK, fill=comp151Colors.PINK)
+        drawing.draw_triangle((600, 0),(700,150), (500,150),
+                              color=comp151Colors.BLACK, fill=comp151Colors.PINK)
+        drawing.draw_circle((450, 200), 100,
+                            color=comp151Colors.BLACK, fill=comp151Colors.PINK)
+        drawing.draw_rectangle((400, 230), (500, 200),
+                               color=comp151Colors.BLACK, fill=comp151Colors.BLACK)
+        drawing.draw_line((300,200), (300,500), color=comp151Colors.BLACK,
+                  thickness=2)
 
 drawing.setup_dearpygui()
 drawing.show_viewport()
